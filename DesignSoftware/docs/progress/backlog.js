a15a29c63a5b5c50e2d855cbc0b0991b7f82a2de
sohale <sohale@gmail.com>
2015-11-05 13:30:16 +0000

    contraints: this.obj.  backlog.js started

---
+++ DesignSoftware/docs/progress/backlog.js
@@ -0,0 +1,19 @@
+
+var list;
+
+var mode = "essential";
+
+j12=t("remove use of dep argument from init_updateannotation", "Dont remove the dep argument yet");
+
+j14=todo(
+    "Refector the root for 1-multiple instances, 2-include print settings, 3-include constraints, 4-include tools, 5-include metadata (owner,time,version)",
+    "clone should work + all of this included in state stack",
+    "11:00am 5 Nov 2015");
+
+
+var mode = "less important";
+
+j13=todo("show list of objects selected in constraints sidebar.", "");
+j14=todo("Checkbox for list of constaints.", "high-level description", "11:00am 5 Nov 2015");
+
+print_all("essential");
